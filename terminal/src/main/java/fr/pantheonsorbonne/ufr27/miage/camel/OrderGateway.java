f6c7a402e8347c7fab187157765a04fc17b20a84
Florent Zheng <florentzheng01@gmail.com>
2022-12-06 00:11:37 +0100

    throwing exceptions in OrderGateway

--- terminal/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/OrderGateway.java
+++ terminal/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/OrderGateway.java
@@ -1,5 +1,7 @@
 package fr.pantheonsorbonne.ufr27.miage.camel;
 
+import fr.pantheonsorbonne.ufr27.miage.exception.OrderNotFoundException;
+import fr.pantheonsorbonne.ufr27.miage.exception.ProductNotFoundException;
 import fr.pantheonsorbonne.ufr27.miage.service.OrderService;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Handler;
@@ -22,26 +24,26 @@ public class OrderGateway {
 
 
     @Handler
-    public Integer createOrder(Integer message){
+    public Integer createOrder(Integer message) throws ProductNotFoundException {
         return  orderService.createOrder(message);
     }
 
     @Handler
-    public Integer addProductOrder(List<Integer> messageBody){
+    public Integer addProductOrder(List<Integer> messageBody) throws OrderNotFoundException, ProductNotFoundException {
         return orderService.addProductOrder(messageBody.get(1),messageBody.get(0));
     }
     @Handler
-    public Float getTotalPrice(Integer message){
+    public Float getTotalPrice(Integer message) throws OrderNotFoundException, ProductNotFoundException {
         return orderService.getTotalPrice(message);
     }
 
     @Handler
-    public void deleteOrder(Integer orderId){
+    public void deleteOrder(Integer orderId) throws OrderNotFoundException, ProductNotFoundException {
         orderService.deleteOrder(orderId);
     }
 
     @Handler
-    public Integer deleteProductOrder(List<Integer> messageBody) {
+    public Integer deleteProductOrder(List<Integer> messageBody) throws OrderNotFoundException, ProductNotFoundException {
         return orderService.deleteProductOrder(messageBody.get(1),messageBody.get(0));
     }
 
