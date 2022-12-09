d17a72966cd4726985e05df2772a04b1c6d86c75
Mehdi Chebbah <i16.chebbah@gmail.com>
2022-12-09 17:39:32 +0100

    fix: REST nomage

--- employee/src/main/java/fr/pantheonsorbonne/ufr27/miage/resources/OrderResource.java
+++ employee/src/main/java/fr/pantheonsorbonne/ufr27/miage/resources/OrderResource.java
@@ -12,26 +12,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
-@Path("client")
-public class ClientResource {
+@Path("order")
+public class OrderResource {
 
     @Inject
     ProductService productService;
     @Inject
     OrderService orderService;
 
-    @Path("products")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllProduct(){
-        //Employee ask terminal for product list
-        //Terminal sends product list
-        //Employee returns product list to client
-
-        return Response.ok(productService.getAllProduct()).build();
-    }
-
-    @Path("order/{productId}")
+    @Path("{productId}")
     @POST
     public Response addProduct(@PathParam("productId") Integer productId) throws ProductNotFoundException {
         //Créer la commande
@@ -40,7 +29,7 @@ public class ClientResource {
         return Response.created(URI.create("order/"+orderService.createOrder(productId))).build();
     }
 
-    @Path("order/{orderId}/add/{productId}")
+    @Path("{orderId}/product/{productId}")
     @PUT
     public Response addProduct(@PathParam("productId") Integer productId, @PathParam("orderId") Integer orderId){
         //Récupére l'order de la bdd du terminal ?
@@ -48,15 +37,17 @@ public class ClientResource {
         return Response.ok(orderService.addProduct(productId,orderId)).build();
     }
 
-    @Path("order/{orderId}/delete/{productId}")
+    @Path("{orderId}/product/{productId}")
     @DELETE
     public Response deleteProduct(@PathParam("productId") Integer productId, @PathParam("orderId") Integer orderId){
         //Récupére l'order de la mémoire locale du terminal
         //Supprimer le produit, update prix etc..
-        return Response.ok(orderService.deleteProduct(orderId,productId)).build();
+        orderService.deleteProduct(orderId,productId);
+        return Response.noContent().build();
+
     }
 
-    @Path("order/{orderId}/delete")
+    @Path("{orderId}/")
     @DELETE
     public void deleterOrder(@PathParam("orderId") Integer orderId) {
         //Delete order in terminal
@@ -67,7 +58,7 @@ public class ClientResource {
         }
     }
 
-    @Path("order/{orderId}/getTotal") //Le client demande à passer au paiement
+    @Path("{orderId}/total") //Le client demande à passer au paiement
     @Produces(MediaType.APPLICATION_JSON)
     @GET
     public Response getOrderPrice(@PathParam("orderId") Integer orderId) throws OrderNotFoundException {
