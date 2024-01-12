439e5a9f64a9afb1c71adbb14fdd94aaec753767
krishnath92 <krish92130@gmail.com>
2024-01-13 00:09:10 +0100

    add files for token

---
+++ banking-bankin/src/main/java/top/nextnet/model/Token.java
@@ -0,0 +1,67 @@
+package top.nextnet.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Token {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id_token", nullable = false)
+    private int idToken;
+    @Basic
+    @Column(name = "id_bank", nullable = false)
+    private int idBank;
+    @Basic
+    @Column(name = "id_account", nullable = false)
+    private int idAccount;
+    @Basic
+    @Column(name = "token", nullable = false, length = 255)
+    private String token;
+
+    public int getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(int idToken) {
+        this.idToken = idToken;
+    }
+
+    public int getIdBank() {
+        return idBank;
+    }
+
+    public void setIdBank(int idBank) {
+        this.idBank = idBank;
+    }
+
+    public int getIdAccount() {
+        return idAccount;
+    }
+
+    public void setIdAccount(int idAccount) {
+        this.idAccount = idAccount;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Token token1 = (Token) o;
+        return idToken == token1.idToken && idBank == token1.idBank && idAccount == token1.idAccount && Objects.equals(token, token1.token);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idToken, idBank, idAccount, token);
+    }
+}
