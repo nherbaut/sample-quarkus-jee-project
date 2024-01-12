902a2efafc3aee2a500d363e9a40d892c9abd2a2
pascal <dupascal00@gmail.com>
2024-01-12 21:46:07 +0100

    add constant

--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/Constante.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/Constante.java
@@ -1,2 +1,10 @@
-package fr.pantheonsorbonne.ufr27.miage.service;public class Constante {
+package fr.pantheonsorbonne.ufr27.miage.service;
+
+public final class Constante {
+    private Constante(){
+    }
+    // For Notification
+    public static final String AUTHORIZATION = "Authorisation";
+    public static final byte VALID = 1;
+    public static final byte EXPIRED = 0;
 }
