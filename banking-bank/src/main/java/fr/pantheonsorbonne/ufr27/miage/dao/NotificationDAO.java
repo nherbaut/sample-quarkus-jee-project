d5f3360badc3a95fd019fe6e3167a7c72bf158a3
pascal <dupascal00@gmail.com>
2024-01-12 23:52:24 +0100

    add notification repository !

--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/NotificationDAO.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/NotificationDAO.java
@@ -1,2 +1,14 @@
-package fr.pantheonsorbonne.ufr27.miage.dao;public interface NotificationDAO {
+package fr.pantheonsorbonne.ufr27.miage.dao;
+
+import fr.pantheonsorbonne.ufr27.miage.exception.NotificationNotFoundException;
+import fr.pantheonsorbonne.ufr27.miage.model.Notification;
+
+import java.sql.Date;
+import java.util.Collection;
+
+public interface NotificationDAO {
+
+    Collection<Notification> findNotificationAuthorisationAvailableForAccount(int idAccount) throws NotificationNotFoundException;
+    Notification updateNotificationEtat(int idNotification, byte etat) throws NotificationNotFoundException;
+    Notification createNewNotification(String texte, byte etat, int idAccount, Date date, String type);
 }
--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/NotificationDAOImpl.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/NotificationDAOImpl.java
@@ -1,2 +1,58 @@
-package fr.pantheonsorbonne.ufr27.miage.dao;public class NotificationDAOImpl {
+package fr.pantheonsorbonne.ufr27.miage.dao;
+
+import fr.pantheonsorbonne.ufr27.miage.exception.NotificationNotFoundException;
+import fr.pantheonsorbonne.ufr27.miage.model.Notification;
+import fr.pantheonsorbonne.ufr27.miage.service.Constante;
+import fr.pantheonsorbonne.ufr27.miage.service.NotificationService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+import java.sql.Date;
+import java.util.Collection;
+
+@ApplicationScoped
+public class NotificationDAOImpl implements NotificationDAO {
+
+    @PersistenceContext(name = "mysql")
+    EntityManager em;
+    @Override
+    @Transactional
+    public Collection<Notification> findNotificationAuthorisationAvailableForAccount(int idAccount) throws NotificationNotFoundException {
+        try{
+            Collection<Notification> notifForAnAccountList = (Collection<Notification>) em.createQuery("Select n from Notification n where n.idAccount=:idAccount and n.type =:type and n.etat=:etat")
+                    .setParameter("idAccount", idAccount)
+                    .setParameter("type", Constante.AUTHORIZATION)
+                    .setParameter("etat", Constante.VALID)
+                    .getResultList();
+            return notifForAnAccountList;
+        } catch (NoResultException e){
+            throw new NotificationNotFoundException();
+        }
+    }
+    @Override
+    @Transactional
+    public Notification updateNotificationEtat(int idNotification, byte etat) throws NotificationNotFoundException {
+        try{
+            Notification notif = em.find(Notification.class, idNotification);
+            if(notif != null){
+                notif.setEtat(etat);
+                em.merge(notif);
+                return notif;
+            }else {
+                throw new NotificationNotFoundException();
+            }
+        }catch(NoResultException e){
+            throw new NotificationNotFoundException();
+        }
+    }
+    @Override
+    @Transactional
+    public Notification createNewNotification(String texte, byte etat, int idAccount, Date date, String type){
+        Notification n = new Notification(texte,etat,idAccount,date,type);
+        em.persist(n);
+        return n;
+    }
 }
