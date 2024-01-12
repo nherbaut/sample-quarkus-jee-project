c31a82046d296de7204a45eaf16663d69715257f
pascal <dupascal00@gmail.com>
2024-01-12 23:48:27 +0100

    add new services !

--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/CustomerService.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/CustomerService.java
@@ -1,2 +1,7 @@
-package fr.pantheonsorbonne.ufr27.miage.service;public interface CustomerService {
+package fr.pantheonsorbonne.ufr27.miage.service;
+
+import fr.pantheonsorbonne.ufr27.miage.model.Customer;
+
+public interface CustomerService {
+    Customer findCustomer(String email);
 }
