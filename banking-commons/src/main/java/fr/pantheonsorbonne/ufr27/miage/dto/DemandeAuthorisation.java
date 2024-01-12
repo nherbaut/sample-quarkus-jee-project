cf71016c04478da117a5111f11711ee31a3ac2be
pascal <dupascal00@gmail.com>
2024-01-12 21:43:22 +0100

    add new entity dto

--- banking-commons/src/main/java/fr/pantheonsorbonne/ufr27/miage/dto/DemandeAuthorisation.java
+++ banking-commons/src/main/java/fr/pantheonsorbonne/ufr27/miage/dto/DemandeAuthorisation.java
@@ -1,2 +1,25 @@
-package fr.pantheonsorbonne.ufr27.miage.dto;public class DemandeAuthorisation {
+package fr.pantheonsorbonne.ufr27.miage.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class DemandeAuthorisation {
+    String texte;
+    User user;
+    public DemandeAuthorisation(User user, String texte){
+        this.user = user;
+        this.texte = texte;
+    }
+    public void setUser(User user){
+        this.user = user;
+    }
+    public User getUser(){
+        return this.user;
+    }
+    public void setTexte(String texte){
+        this.texte = texte;
+    }
+    public String getTexte(){
+        return this.texte;
+    }
 }
