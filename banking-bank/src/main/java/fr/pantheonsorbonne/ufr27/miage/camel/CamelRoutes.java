d1762d032af2bf88b510d45db5b44e5c0a51b692
pascal <dupascal00@gmail.com>
2024-01-13 15:33:54 +0100

    add notification gateway and new routes !

--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
@@ -1,8 +1,13 @@
 package fr.pantheonsorbonne.ufr27.miage.camel;
 
 
+import fr.pantheonsorbonne.ufr27.miage.dto.DemandeAuthorisation;
 import fr.pantheonsorbonne.ufr27.miage.exception.BankAccountNotFoundException;
 import fr.pantheonsorbonne.ufr27.miage.exception.BankCustomerNotFoundException;
+import fr.pantheonsorbonne.ufr27.miage.exception.NotificationFoundException;
+import fr.pantheonsorbonne.ufr27.miage.service.CompteService;
+import fr.pantheonsorbonne.ufr27.miage.service.Constante;
+import fr.pantheonsorbonne.ufr27.miage.service.NotificationService;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -21,6 +26,12 @@ public class CamelRoutes extends RouteBuilder {
     String bankName;
     @ConfigProperty(name = "fr.pantheonsorbonne.ufr27.miage.clientEmail")
     String clientEmail;
+    @ConfigProperty(name = "fr.pantheonsorbonne.ufr27.miage.bankGroup")
+    String bankGroup;
+    @Inject
+    CompteService compteService;
+    @Inject
+    NotificationService notificationService;
     @Inject
     CamelContext camelContext;
 
@@ -38,6 +49,48 @@ public class CamelRoutes extends RouteBuilder {
                 .handled(true)
                 .setHeader("success",simple("false"))
                 .setBody(simple("Customer Not found"));
+
+        onException(NotificationFoundException.NotificationAuthorisationFoundException.class)
+                .handled(true)
+                .setHeader("success",simple("false"))
+                .setBody(simple("Request has already been sent for today ! It will expire in the next 24 hours"));
+
+        from("sjms2:"+jmsPrefix+"requestSynch?exchangePattern=InOut")
+                .autoStartup(isRouteEnabled)
+                .filter(header("bankName").isEqualTo(bankName))
+                .filter(header("clientEmail").isEqualTo(clientEmail))
+                .choice()
+                .when(header("bankGroup").isEqualTo(Constante.BPCE_GROUP_BANK))
+                .unmarshal().json(DemandeAuthorisation.class)
+                .endChoice()
+                .otherwise()
+                .unmarshal().jacksonXml(DemandeAuthorisation.class)
+                .end()
+                .bean(compteService,"login")
+                .log("Connection success ! verify if you have already sent a synchronization request for today ...")
+                .bean(notificationService,"verifyNotificationCreated")
+                .log("Sending notification for user ...")
+                .bean(notificationService,"newNotification")
+                .setHeader("success",simple("true"))
+                .choice()
+                .when(header("bankGroup").isEqualTo(Constante.BPCE_GROUP_BANK))
+                .marshal().json()
+                .endChoice()
+                .otherwise()
+                .marshal().jacksonXml();
+
+
+    from("direct:responseSynchro")
+                .autoStartup(isRouteEnabled)
+                .setHeader("bankGroup",simple(bankGroup))
+                .setHeader("bankName",simple(bankName))
+                .choice()
+                .when(header("bankGroup").isEqualTo(Constante.BPCE_GROUP_BANK))
+                .marshal().json()
+                .to("sjms2:topic:" + jmsPrefix + "respondSynchro")
+                .otherwise()
+                .marshal().jacksonXml()
+                .to("sjms2:topic:" + jmsPrefix + "respondSynchro");
 
     }
 }
--- banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/NotificationGateway.java
+++ banking-bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/NotificationGateway.java
@@ -1,7 +1,36 @@
 package fr.pantheonsorbonne.ufr27.miage.camel;
 
+import fr.pantheonsorbonne.ufr27.miage.dto.DemandeAuthorisation;
+import fr.pantheonsorbonne.ufr27.miage.model.Notification;
+import fr.pantheonsorbonne.ufr27.miage.service.NotificationService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.IOException;
 
 @ApplicationScoped
 public class NotificationGateway {
+    @ConfigProperty(name = "fr.pantheonsorbonne.ufr27.miage.jmsPrefix")
+    String jmsPrefix;
+    @Inject
+    NotificationService notificationService;
+    @Inject
+    CamelContext camelContext;
+
+    public void sendResponseSynchro(String response, Notification n){
+        try (ProducerTemplate producerTemplate = camelContext.createProducerTemplate()) {
+            if(response == "Yes"){
+                producerTemplate.sendBodyAndHeader("direct:responseSynchro", "Authorization accepted ! token generated", "success","true");
+                //here we can send the token to bankin, also all information related to the customer now
+            }else{
+                producerTemplate.sendBodyAndHeader("direct:responseSynchro","Authorization refused","success","false");
+            }
+            notificationService.updateNotificationHandle(n.getIdNotification());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
